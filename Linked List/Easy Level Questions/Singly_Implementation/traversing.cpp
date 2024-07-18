064c28b42217e51c3e47cc3aa94cc46e90bf82f2
Sajal Prajapati <114945501+sajalprajapati@users.noreply.github.com>
2024-07-18 11:51:31 +0530

    traversing the singly linked list

---
+++ Linked List/Easy Level Questions/Singly_Implementation/traversing.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node *next;
+
+    node(int data)
+    {
+        this->data=data;
+        this->next=nullptr;
+    }
+};
+
+node* CreateLinkedList()
+{
+    int number_of_node;
+    cout<<"\n tell me how many nodes of singly linked list we want to create ..now"<<endl;
+    cin>>number_of_node;
+
+   node* head=nullptr;
+   node* tail=nullptr;
+
+   for(int i=0;i<number_of_node;i++)
+   {
+    int data;
+    cout<<"\n what data we want to insert in the node"<<endl;
+    cin>>data;
+
+    node* newnode=new node(data); //this is dynamically we are creating the node and storing it in the memory;
+
+    if(head==nullptr)
+    {
+        head=newnode;
+        tail=newnode;
+    }
+    else
+    {
+        tail->next=newnode;
+        tail=newnode;
+    }
+   }
+
+   return head;
+}
+
+void traversingSinglyList(node *head)
+{
+    node* current=head;
+
+    while(current!=nullptr)
+    {
+        cout<<current->data<<" ";
+        current=current->next;
+    }
+
+    if(head==nullptr)
+    {
+        cout<<"NULL"<<endl;
+    }
+}
+
+int main()
+{
+    node *root=CreateLinkedList();
+
+    //now we want to print the element from the linked list ..
+    traversingSinglyList(root);
+    
+   
+    return 0;
+}
