ff58bcbb45246ae6f1fa748d712048b258e8d2fe
Sajal Prajapati <114945501+sajalprajapati@users.noreply.github.com>
2024-07-19 15:40:12 +0530

    finding the length of the loop present

---
+++ Linked List/Easy Level Questions/Singly_Implementation/Find the length of the loop present/hashing.cpp
@@ -0,0 +1,89 @@
+
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Node class represents a
+// node in a linked list
+class Node {
+public:
+    // Data stored in the node
+    int data;   
+    
+    // Pointer to the next node in the list
+    Node* next;      
+
+    // Constructor with both data and
+    // next node as parameters
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    // Constructor with only data as a
+    // parameter, sets next to nullptr
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+// Function to return the length 
+// of loop using hashing
+
+int lengthOfLoop(Node* head) 
+{
+   unordered_map<node *,int> mp;
+
+   node *current=head;
+   int timer=0;
+
+   while(current!=nullptr) 
+   {
+    if(mp.find(current)!=mp.end())
+    {
+        return timer-mp[current];//this will be our lenght that is being provided to us..
+    }
+
+    mp[current]=timer;
+    current=current->next;
+    timer++;
+   }
+
+   return 0;
+}
+
+
+
+int main() {
+    // Create a sample linked
+    // list with a loop
+    
+    Node* head = new Node(1);
+    Node* second = new Node(2);
+    Node* third = new Node(3);
+    Node* fourth = new Node(4);
+    Node* fifth = new Node(5);
+
+    // Create a loop from
+    // fifth to second
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+    fourth->next = fifth;
+    
+    // This creates a loop
+    fifth->next = second; 
+
+    int loopLength = lengthOfLoop(head);
+    if (loopLength > 0) {
+        cout << "Length of the loop: " << loopLength << endl;
+    } else {
+        cout << "No loop found in the linked list." << endl;
+    }
+
+    return 0;
+}
+
+
