13f315233b3579a0cd68fe5ce1ad8d20c7108688
Sajal Prajapati <114945501+sajalprajapati@users.noreply.github.com>
2024-07-23 14:41:20 +0530

    reverse the linked list

---
+++ Linked List/Easy Level Questions/Singly_Implementation/Reverse_the_linked_list_using_recursion.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node* next;
+
+    node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
+};
+
+node* CreateLinkedList() {
+    int number_of_node;
+    cout << "\nTell me how many nodes of singly linked list we want to create now" << endl;
+    cin >> number_of_node;
+
+    node* head = nullptr;
+    node* tail = nullptr;
+
+    for (int i = 0; i < number_of_node; i++) {
+        int data;
+        cout << "\nWhat data do we want to insert in the node" << endl;
+        cin >> data;
+
+        node* newnode = new node(data); // Dynamically creating the node and storing it in memory
+
+        if (head == nullptr) {
+            head = newnode;
+            tail = newnode;
+        } else {
+            tail->next = newnode;
+            tail = newnode;
+        }
+    }
+
+    return head;
+}
+
+void traversingSinglyList(node* head) {
+    node* current = head;
+
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+
+    cout << "NULL" << endl; // Ensure to print NULL at the end of the list
+}
+
+node* reverse(node* head) 
+{
+  if(head==nullptr or head->next==nullptr)
+  return head;
+
+  node* newHead=reverse(head->next);
+  node* front=head->next;
+  front->next=head;
+  head->next=nullptr;
+  return newHead;
+    
+}
+
+int main() {
+    node* root = CreateLinkedList();
+
+    // Print the original linked list
+    cout << "Original list: ";
+    traversingSinglyList(root);
+
+    // Reverse the linked list
+    node* reverse_head = reverse(root);
+
+    // Print the reversed linked list
+    cout << "Reversed list: ";
+    traversingSinglyList(reverse_head);
+
+    return 0;
+}
