952519c6d4d31d8ce4f7ca3b8498a9a74c742153
Sajal Prajapati <114945501+sajalprajapati@users.noreply.github.com>
2024-07-23 12:41:22 +0530

    reverse the doubly linked list

---
+++ Linked List/Easy Level Questions/Doubly Linked List/Reverse the doubly linked list/my intuition.cpp
@@ -0,0 +1,93 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node
+{
+    public:
+    int data;
+    node* next;
+    node* previous;
+
+    node(int data)
+    {
+        this->data=data;
+        this->next=nullptr;
+        this->previous=nullptr;
+    }
+};
+
+node* createDoublyLinkedList()
+{
+    int numberOfNodes;
+    cout<<"\n enter the number of nodes want to create"<<endl;
+    cin>>numberOfNodes;
+
+    if(numberOfNodes<=0)
+    {
+        cout<<"\n no creation of the linked list as the number of nodes is zero"<<endl;
+        return nullptr;
+    }
+
+   node* head=nullptr;
+   node* tail=nullptr;
+   for(int i=0;i<numberOfNodes;i++)
+   {
+     int data;
+     cout<<"\n enter the data for the node -"<<i;
+     cin>>data;
+    
+     node* newnode=new node(data);
+
+     if(head==nullptr)
+     {
+        head=newnode;
+        tail=newnode;
+     }
+     else
+     {
+        tail->next=newnode;
+        newnode->previous=tail;
+        tail=newnode;
+     }
+   }
+
+   return head;
+}
+
+
+void traversal(node *head)
+{
+    if(head==nullptr)
+    {
+        return;
+    }
+
+    cout<<head->data<<" ";
+    traversal(head->next);
+}
+
+
+node* reversal(node* head)
+{
+    if(head==nullptr)
+        return head;
+        
+        if(head->next==nullptr)
+        return head;
+        
+        node* current=head;
+        
+        while(current!=nullptr)
+        {
+            swap(current->previous,current->next);
+            if(current->previous==nullptr)
+            head=current;
+            current=current->previous;
+        }
+        
+        return head;
+}
+int main()
+{
+    node* root=createDoublyLinkedList();
+    traversal(reversal(root));
+}
