9fb83d68592086fb4520e2c4b52046f3b4ec3ba3
Sajal Prajapati <114945501+sajalprajapati@users.noreply.github.com>
2024-07-24 21:20:08 +0530

    deletion of the duplicates in the doubly linked list

---
+++ Linked List/Easy Level Questions/Doubly Linked List/Delete the duplicates in the doubly linked list/sorting based deletion in the doubly linked list.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node {
+public:
+    int data;
+    node* next;
+    node* previous;
+
+    node(int data) {
+        this->data = data;
+        this->next = nullptr;
+        this->previous = nullptr;
+    }
+};
+
+node* createDoublyLinkedList() {
+    int numberOfNodes;
+    cout << "\nEnter the number of nodes you want to create: ";
+    cin >> numberOfNodes;
+
+    if (numberOfNodes <= 0) {
+        cout << "\nNo creation of the linked list as the number of nodes is zero or negative" << endl;
+        return nullptr;
+    }
+
+    node* head = nullptr;
+    node* tail = nullptr;
+    for (int i = 0; i < numberOfNodes; i++) {
+        int data;
+        cout << "Enter the data for the node " << i + 1 << ": ";
+        cin >> data;
+
+        node* newNode = new node(data);
+
+        if (head == nullptr) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail->next = newNode;
+            newNode->previous = tail;
+            tail = newNode;
+        }
+    }
+
+    return head;
+}
+
+void traversal(node* head) {
+    if (head == nullptr) {
+        cout << "List is empty" << endl;
+        return;
+    }
+
+    node* current = head;
+    while (current != nullptr) {
+        cout << current->data << " ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+node* deleteDoublyLinkedList(node* head) 
+{
+  if(head==nullptr or head->next==nullptr)
+  return head;
+
+ node* temp=head;
+ while(temp!=nullptr and temp->next!=nullptr)
+ {
+    node *newnode=temp->next;
+    while(newnode->data==temp->data and newnode!=nullptr)
+    {
+        node* duplicate=newnode;
+        newnode=newnode->next;
+        delete(duplicate);
+    }
+
+    temp->next=newnode;
+    if(newnode!=nullptr) newnode->previous=temp;
+    temp=temp->next;
+
+ }
+  return head;  
+}
+
+int main() 
+{
+    node* root = createDoublyLinkedList();
+    traversal( deleteDoublyLinkedList(root));
+    return 0;
+}
