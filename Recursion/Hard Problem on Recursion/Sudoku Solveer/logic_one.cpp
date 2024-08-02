8e88af7083956f30100843d0e2e052ba58adeb0d
Sajal Prajapati <114945501+sajalprajapati@users.noreply.github.com>
2024-08-02 12:06:36 +0530

    sudko solving

--- Recursion/Hard Problem on Recursion/Sudoku Solveer/logic_one.cpp
+++ Recursion/Hard Problem on Recursion/Sudoku Solveer/logic_one.cpp
@@ -51,7 +51,75 @@ bool validBox(vector<vector<char>>& board,int startingRow,int endingRow,int star
       }
 
 
-      //validating 3*3 box;
+      //validating 3*3 box;#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int row, int col, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch) return false; // Check row
+            if (board[i][col] == ch) return false; // Check column
+            if (board[row / 3 * 3 + i / 3][col / 3 * 3 + i % 3] == ch) return false; // Check 3x3 sub-box
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (isValid(board, i, j, ch)) {
+                            board[i][j] = ch;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
+
+int main() {
+    vector<vector<char>> board = {
+        {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+        {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+        {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+        {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+        {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+        {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+        {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+        {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+        {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+    };
+
+    Solution solution;
+    solution.solveSudoku(board);
+
+    for (const auto& row : board) {
+        for (const auto& ch : row) {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
       for(int startingRow=0;i<9;i+3)
       {
         int endingRow=startingRow+2;
