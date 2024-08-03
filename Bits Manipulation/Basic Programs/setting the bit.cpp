df59dd9eff68c6664fb5d48f22e32ff262d26442
Sajal Prajapati <114945501+sajalprajapati@users.noreply.github.com>
2024-08-03 15:26:31 +0530

    setting the bit

---
+++ Bits Manipulation/Basic Programs/setting the bit.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int setting(int &a) {
+    int i;
+    cout << "\nEnter the value of i: ";
+    cin >> i;
+    
+    // Correct the condition using parentheses
+    a=(a | (1 << i)) ;
+    return a;
+       
+}
+
+int main() {
+    int a = 9;
+    bool answer = setting(a);
+    
+    cout<<a<<" "<<endl;
+    
+    return 0;
+}
